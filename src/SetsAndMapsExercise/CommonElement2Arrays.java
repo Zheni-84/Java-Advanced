3da981a970228c1f3435a333153a9df0760f7cc9
Zhelyazka Avramova <z.avramova@tinqin.com>
2022-05-28 21:10:07 +0300

    Sets and Maps Exercise

---
+++ src/SetsAndMapsExercise/CommonElement2Arrays.java
@@ -0,0 +1,36 @@
+package SetsAndMapsExercise;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+//https://pastebin.com/88mYHps0
+public class CommonElement2Arrays {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int[] firstArr = Arrays.stream(sc.nextLine().split("\\s+"))
+				.mapToInt(Integer::parseInt)
+				.toArray();
+		int[] secondArr = Arrays.stream(sc.nextLine().split("\\s+"))
+				.mapToInt(Integer::parseInt)
+				.toArray();
+
+		if (firstArr.length == 0 || secondArr.length == 0) {
+			System.out.println("None common elements");
+		}
+		HashSet<Integer> set = new HashSet<>();
+		for (int num : firstArr) {
+			set.add(num);
+		}
+		for (int num : secondArr) {
+			if (set.contains(num)) {
+				System.out.println(num);
+			}
+		}
+	}
+
+}
---
+++ src/SetsAndMapsExercise/SetsOfElements.java
@@ -0,0 +1,30 @@
+package SetsAndMapsExercise;
+
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+
+public class SetsOfElements {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int[] setSizes = Arrays.stream(sc.nextLine().split("\\s+"))
+				.mapToInt(Integer::parseInt)
+				.toArray();
+		int firstSize = setSizes[0];
+		LinkedHashSet<String> firstSet = new LinkedHashSet<>();
+		for (int i = 0; i < firstSize; i++) {
+			firstSet.add(sc.nextLine());
+		}
+		int secondSize = setSizes[1];
+		LinkedHashSet<String> secondSet = new LinkedHashSet<>();
+		for (int i = 0; i < secondSize; i++) {
+			secondSet.add(sc.nextLine());
+		}
+
+		firstSet.retainAll(secondSet);
+		System.out.println(String.join(" ", firstSet));
+	}
+
+}
